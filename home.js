b85a672c564220d32263bf1abe88b69c0c843744
Demeyer10 <04_zoster_borscht@icloud.com>
2024-12-04 13:19:00 -0500

    Experience to Home animations

--- home.js
+++ home.js
@@ -1,3 +1,6 @@
+var current = "Home"
+var previous = "Home"
+
 window.addEventListener('load', function() {
     let width = window.innerWidth;
     // Hide the loading overlay and show the main content
@@ -11,33 +14,64 @@ window.addEventListener('load', function() {
   });
 
 
-  function loadExperience() {
-    document.getElementById('pic-column').style.opacity = '0';
-    document.getElementById('about').style.opacity = '0';
-    document.getElementById('full-stack').style.opacity = '0';
-    document.getElementById('social').style.opacity = '0';
+function loadExperience() {
+
+    var current = "Experience"
+
+    document.getElementById('pic-column').style.transform = 'translateY(-100vh)';
+    document.getElementById('about').style.transform = 'translateX(-100vw)';
+    document.getElementById('full-stack').style.transform = 'translateX(-100vw)';
+    document.getElementById('social').style.transform = 'translateY(100vh)';
 
 
 
     setTimeout(() => {
-        document.getElementById('test').style.display = 'flex';
+        document.getElementById('experiences').style.display = 'flex';
         document.getElementById('about').style.display = 'None';
-        document.getElementById('test').style.transform = 'translateX(100vw))';
+        document.getElementById('experiences').style.transform = 'translateX(100vw))';
         document.getElementById('pic-column').style.display = 'None';
         document.getElementById('social').style.display = 'None';
-      }, 1000);
+        setTimeout(() => {
+          document.getElementById('experiences').style.transform = 'translateX(0)';
+        }, 100);
+      }, 400);
 
 
 
-      setTimeout(() => {
-        document.getElementById('test').style.transform = 'translateX(0)';
-      }, 1300);
+
+
+}
+
+function loadHome() {
+
+  document.getElementById('experiences').style.transform = 'translateX(-100vw)';
 
 
 
-    
+  setTimeout(() => {
+    document.getElementById('experiences').style.display = 'None';
+    document.getElementById('about').style.display = 'flex';
+    document.getElementById('pic-column').style.display = 'flex';
+    document.getElementById('full-stack').style.display = 'flex';
+    document.getElementById('social').style.display = 'flex';
+
+    setTimeout(() => {
+
+      document.getElementById('about').style.transform = 'translateX(0)';
+      document.getElementById('pic-column').style.transform = 'translateY(0)';
+      document.getElementById('social').style.transform = 'translateY(0)';
+      document.getElementById('full-stack').style.transform = 'translateY(0)';
+    }, 200);
+
+
+  }, 400);
 
 
 
+
+
+
+  if (current === "Experience"){
 
   }
+}
